cc2dcc6a3d3542a9f1e501fef77667f01a02de46
Abhishek SIngh <abhishekmithileshsingh1@gmail.com>
2022-01-07 13:02:02 +0530

    Mixed match (MIstake)

--- Function1.cpp
+++ Function1.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 
 void doit(); // Function declaration
-
+int abhi(int i1n);
+//int foo(int);
 
 int main()
 {
@@ -18,6 +19,16 @@ int main()
 void doit() {   // Function Defination
     std::cout << "Starting the task ...." << std::endl;
     std::cout << "completed the task ...." << std::endl;
-
+    std::cout << "Adding i1n two times : " << abhi(45) << std::endl;
+    // foo(45);
     return;
 }
+
+
+int abhi(int i1n) {
+    return i1n + i1n;
+}
+
+//float foo(float) {                     //Mixed match
+//    return 1.3f;
+//}
